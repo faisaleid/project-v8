acd1aeebb6aa6f5a7bff90dbca1a00c376735983
faisaleid <faisaleid5@hotmail.com>
2021-08-08 15:16:53 +0300

    Third commit

--- controllers/vaccinationEvents.js
+++ controllers/vaccinationEvents.js
@@ -15,4 +15,51 @@ const addvaccinationEvents = (req, res) => {
   });
 };
 
-module.exports = { addvaccinationEvents };
+const getvaccinationEvents = (req, res) => {
+  vaccinationEventsSchema.find({}, (err, event) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(event);
+  });
+};
+
+const updateEvent = (req, res) => {
+  vaccinationEventsSchema.findOneAndUpdate(
+    { _id: req.params.EventID },
+    req.body,
+    { new: true, useFindAndModify: false },
+    (err, event) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(event);
+    }
+  );
+};
+
+const deleteEvent = (req, res) => {
+  vaccinationEventsSchema.remove({ _id: req.params.EventID }, (err, event) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: "successfuly deleted the Event" });
+  });
+};
+
+const getvaccinationEventbyID = (req, res) => {
+  vaccinationEventsSchema.findById(req.params.EventID, (err, event) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(event);
+  });
+};
+
+module.exports = {
+  addvaccinationEvents,
+  getvaccinationEvents,
+  updateEvent,
+  deleteEvent,
+  getvaccinationEventbyID,
+};
--- routes/vaccinationEvents.js
+++ routes/vaccinationEvents.js
@@ -1,11 +1,30 @@
 const controllers = require("../controllers/vaccinationEvents");
 const checkAuth = require("../middleware/check-auth");
 const addvaccinationEvents = controllers.addvaccinationEvents;
+const getvaccinationEvents = controllers.getvaccinationEvents;
+const updateEvent = controllers.updateEvent;
+const deleteEvent = controllers.deleteEvent;
+const getvaccinationEventbyID = controllers.getvaccinationEventbyID;
 
 const routes = (app) => {
+  app.route("/vaccinationEvents").get(
+    (req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    },
+    checkAuth,
+    getvaccinationEvents
+  );
   app
     .route("/vaccinationEvents/addvaccinationEvents")
     .post(checkAuth, addvaccinationEvents);
+  app
+    .route("/vaccinationEvents/:EventID")
+    .put(checkAuth, updateEvent)
+    .delete(checkAuth, deleteEvent)
+    .get(checkAuth, getvaccinationEventbyID);
 };
 
 module.exports = routes;
